87fb5dc11de44f04ddfdcbb9e31ba4183928134e
Alice Cecil <alycecil@gmail.com>
2018-09-15 03:20:17 -0400

    Release 1

---
+++ Source/mod/harmony/PawnGenerator_GeneratePawn_Patch.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Linq;
+using Harmony;
+using RimWorld;
+using Verse;
+
+namespace DarkIntentionsWoohoo.harmony
+{
+    [HarmonyPatch(typeof(PawnGenerator), "GeneratePawn", typeof(PawnGenerationRequest))]
+        public static class PawnGenerator_GeneratePawn_Patch
+        {
+            
+
+            [HarmonyPostfix]
+            public static void Postfix(ref Pawn __result, PawnGenerationRequest request)
+            {
+                //Amnesty for children
+                if(__result?.guest != null && __result.guest.IsPrisoner && __result.guest.HostFaction == Faction.OfPlayer) __result.guest.SetGuestStatus(null);
+            }
+        }
+}
